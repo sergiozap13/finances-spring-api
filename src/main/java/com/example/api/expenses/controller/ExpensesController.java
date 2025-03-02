d5b479606732c8fac30996d52f29c3b172473d61
sergio zapata <sergiozap1@gmail.com>
2025-03-02 12:32:42 +0100

    Add CORS to controllers

--- src/main/java/com/example/api/expenses/controller/ExpensesController.java
+++ src/main/java/com/example/api/expenses/controller/ExpensesController.java
@@ -3,7 +3,7 @@ package com.example.api.expenses.controller;
 import com.example.api.expenses.dto.ExpenseDTO;
 import com.example.api.expenses.model.Expense;
 import com.example.api.expenses.service.ExpensesService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,11 +12,13 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/gastos")
+@AllArgsConstructor
 public class ExpensesController {
 
-    @Autowired
+    // se autoinyecta en el constructor con todos los args
     private ExpensesService expensesService;
 
     @GetMapping
@@ -41,12 +43,8 @@ public class ExpensesController {
 
     @GetMapping("/{id}")
     public ResponseEntity<Expense> obtenerGastoPorId(@PathVariable UUID id) {
-        try {
-            Expense expense = expensesService.obtenerPorId(id);
-            return new ResponseEntity<>(expense, HttpStatus.OK);
-        } catch (RuntimeException e) {
-            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-        }
+        Expense expense = expensesService.obtenerPorId(id);
+        return new ResponseEntity<>(expense, HttpStatus.OK);
     }
 
     @PostMapping
@@ -56,23 +54,14 @@ public class ExpensesController {
 
     @PutMapping("/{id}")
     public ResponseEntity<Expense> actualizarGasto(@PathVariable UUID id,@RequestBody ExpenseDTO expenseDTO){
-        try {
-            Expense expenseToUpdate = this.expensesService.actualizarGasto(id,expenseDTO);
-            return new ResponseEntity<>( expenseToUpdate, HttpStatus.OK );
-
-        } catch (RuntimeException e) {
-        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-        }
+        Expense expenseToUpdate = this.expensesService.actualizarGasto(id,expenseDTO);
+        return new ResponseEntity<>( expenseToUpdate, HttpStatus.OK );
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteGasto(@PathVariable UUID id) {
-        try {
-            this.expensesService.eliminarGasto(id);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } catch (RuntimeException e){
-            return new ResponseEntity<>( HttpStatus.NOT_FOUND );
-        }
+        this.expensesService.eliminarGasto(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 
--- src/main/java/com/example/api/users/controller/UsuarioController.java
+++ src/main/java/com/example/api/users/controller/UsuarioController.java
@@ -3,19 +3,20 @@ package com.example.api.users.controller;
 import com.example.api.users.dto.UsuarioDTO;
 import com.example.api.users.model.Usuario;
 import com.example.api.users.service.UsuarioService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/usuarios")
+@AllArgsConstructor
 public class UsuarioController {
 
-    // inyectamos nuestro servicio
-    @Autowired
+    // inyectamos nuestro servicio con lombok
     private UsuarioService usuarioService;
 
     // Obtener todos los usuarios
@@ -27,12 +28,8 @@ public class UsuarioController {
     // Obtener un usuario por su ID
     @GetMapping("/{id}")
     public ResponseEntity<Usuario> obtenerPorId(@PathVariable UUID id) {
-        try {
-            Usuario usuario = usuarioService.obtenerPorId(id);
-            return new ResponseEntity<>(usuario, HttpStatus.OK);
-        } catch (RuntimeException e) {
-            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-        }
+        Usuario usuario = usuarioService.obtenerPorId(id);
+        return new ResponseEntity<>(usuario, HttpStatus.OK);
     }
 
     // Crear un nuevo usuario
@@ -45,22 +42,14 @@ public class UsuarioController {
     // Actualizar un usuario existente
     @PutMapping("/{id}")
     public ResponseEntity<Usuario> actualizarUsuario(@PathVariable UUID id, @RequestBody UsuarioDTO usuarioDTO) {
-        try {
-            Usuario usuarioActualizado = usuarioService.actualizarUsuario(id, usuarioDTO);
-            return new ResponseEntity<>(usuarioActualizado, HttpStatus.OK);
-        } catch (RuntimeException e) {
-            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-        }
+        Usuario usuarioActualizado = usuarioService.actualizarUsuario(id, usuarioDTO);
+        return new ResponseEntity<>(usuarioActualizado, HttpStatus.OK);
     }
 
     // Eliminar un usuario por su ID
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> eliminarUsuario(@PathVariable UUID id) {
-        try {
-            usuarioService.eliminarUsuario(id);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } catch (RuntimeException e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+        usuarioService.eliminarUsuario(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
